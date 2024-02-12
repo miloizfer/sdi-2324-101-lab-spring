c34163633ca8d88a76d1a4dcb79b1f30898ae930
miloizfer <uo257691@uniovi.es>
2024-02-12 19:34:14 +0100

    SDI-2324-101-2.2-Trabajando con Controladores.

---
+++ notaneitor/src/main/java/com/uniovi/sdi/notaneitor/Sdi2324101SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi.notaneitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324101SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi2324101SpringApplication.class, args);
+	}
+
+}
